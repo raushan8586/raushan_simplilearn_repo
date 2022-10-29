b4481ccd5f1aa2a33eab24d3fc0100d738190664
raushan8586 <72825046+raushan8586@users.noreply.github.com>
2022-10-29 12:01:43 +0530

    terraform

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA4ENX2KXVO5KEG3PN"
+  secret_key = "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"
+}
+
+# create a VPC
+resource "aws_vpc" "simplilearn-vpc1" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "simplilearn-vpc1"
+  }
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "simplilearn-bucket3-29th-oct"
+
+  tags = {
+    Name        = "simplilearn-bucket3-29th-oct"
+    Environment = "Dev"
+  }
+}
